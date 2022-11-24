484928647a90c68e455fdebda376827e30131aee
devfrank9 <devfrank9@gmail.com>
2022-11-24 20:37:36 +0900

    서비스 모듈을 통해 동작? 을 정의하고 리졸버를 통해 클라이언트 등에서 쓸수있게 보내준다

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,12 +1,15 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateRestaurantDto } from './dtos/create-restaurants.dto';
 import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantsService } from './restaurants.service';
 
 @Resolver((of) => Restaurant)
 export class RestaurantsResolver {
+  constructor(private readonly restaurantsService: RestaurantsService) {}
+
   @Query((returns) => [Restaurant])
-  restaurant(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    return [];
+  restaurant(): Promise<Restaurant[]> {
+    return this.restaurantsService.getAll();
   }
 
   @Mutation((returns) => Boolean)
