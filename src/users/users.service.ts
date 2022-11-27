5d29e99d375729da764630b1c547cae6bdd675f5
devfrank9 <devfrank9@gmail.com>
2022-11-27 22:38:41 +0900

    :sparkles: 유저생성 기능

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { CreateAccountInput } from './dtos/create-account.dto';
 
 @Injectable()
 export class UsersService {
@@ -9,4 +10,20 @@ export class UsersService {
     @InjectRepository(User)
     private readonly users: Repository<User>,
   ) {}
+
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<string | undefined> {
+    try {
+      const exists = await this.users.findOne({ email });
+      if (exists) {
+        return 'There is a user with that email already';
+      }
+      await this.users.save(this.users.create({ email, password, role }));
+    } catch (error) {
+      return "Couldn't create account";
+    }
+  }
 }
